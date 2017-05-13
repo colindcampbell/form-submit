aba20463cc162d95406dd40983ec9e070473b31d
Colin Campbell <colin.campbell@logicmonitor.com>
2017-05-13 14:55:57 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var formApp = (function ($, window, document, undefined) {
+	var validator={},
+			formSubmit={};
+
+	validator.initValidate = function(formID){
+		var form = document.getElementById(formID)
+	}
+
+
+
+ }());
